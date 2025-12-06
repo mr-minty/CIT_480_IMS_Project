bf024145417778b4d98003461622dbbf802fa750
mr-minty <crtrwd4@gmail.com>
2025-12-06 00:04:21 -0800

    Added mobile friendly responsive code for the dashboard page.

---
+++ public/js/header-client.js
@@ -0,0 +1,8 @@
+function showDropdownMenu() {
+  var x = document.getElementById("dropdownMenu");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
