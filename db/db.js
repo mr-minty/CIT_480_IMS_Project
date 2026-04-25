a3181ea036d110414c674a088bea5dc230e34066
mr-minty <crtrwd4@gmail.com>
2026-04-25 13:38:44 -0700

    Modified db.js to allow for secure or insecure myysql connections. General CSS cleanups of old sections and unused descriptors.

--- db/db.js
+++ db/db.js
@@ -3,7 +3,9 @@ require("dotenv").config();
 const fs = require("fs");
 const mysql = require("mysql2");
 
-const pool = mysql.createPool({
+const isProd = process.env.NODE_ENV === "production";
+
+const poolConfig = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
@@ -11,12 +13,17 @@ const pool = mysql.createPool({
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
-    ssl: {
+};
+
+// Only add SSL in production
+if (isProd) {
+    poolConfig.ssl = {
         ca: fs.readFileSync("/var/www/Capstone_IMS/certs/global-bundle.pem"),
         rejectUnauthorized: true
-    }
-});
+    };
+}
 
+const pool = mysql.createPool(poolConfig);
 const promisePool = pool.promise();
 
 module.exports = promisePool;
