23d7eaa8fa7f1405af57b03721fe132d0f732834
mr-minty <crtrwd4@gmail.com>
2026-04-25 16:42:56 -0700

    Fixed the dropdown menu display on mobile

--- public/js/header-client.js
+++ public/js/header-client.js
@@ -5,10 +5,12 @@ dropdownButton.addEventListener("click", async (e) => {
   e.stopPropagation();
 
   dropdownMenu.classList.toggle("show");
+  document.body.classList.toggle("menu-open");
 });
 
 document.addEventListener("click", (e) => {
   if(!dropdownnWrapper.contains(e.target)){
     dropdownMenu.classList.remove("show");
+    document.body.classList.remove("menu-open");
   }
 });
