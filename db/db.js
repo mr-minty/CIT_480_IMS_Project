77dd6caf27daf66e219f4ad2c54a8c7de441fa19
mr-minty <crtrwd4@gmail.com>
2026-04-24 20:24:11 -0700

    Fixed ssl for db/db.js

--- db/db.js
+++ db/db.js
@@ -1,19 +1,22 @@
-require('dotenv').config();
+require("dotenv").config();
 
+const fs = require("fs");
 const mysql = require("mysql2");
 
-//Create connection pool
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     waitForConnections: true,
-    connectionLimit: 10,  
-    queueLimit: 0         
+    connectionLimit: 10,
+    queueLimit: 0,
+    ssl: {
+        ca: fs.readFileSync("/var/www/Capstone_IMS/certs/global-bundle.pem"),
+        rejectUnauthorized: true
+    }
 });
 
 const promisePool = pool.promise();
 
 module.exports = promisePool;
-
